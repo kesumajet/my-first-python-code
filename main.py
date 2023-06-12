44590f686a6ca9fa3df262aec4c9f49dd57ca943
Kusuma <kusumo999666@gmail.com>
2023-06-12 20:26:47 +0700

    initial commit

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ This is project demo with python
 print('hello world')
 milk_base = 562
 egg_bake = 25
+print(f'jumlah botol susu {milk_base} bottle')
+print(f'jumlah telur {egg_bake} glundung')
 
 # sequential
 if milk_base > 0:
-    print('budi buy a milk')
+    print('boy check price, his money enough')
+    print('boy buy 1 milk')
+    if egg_bake > 0:
+        print('honestly eggs is available')
+        print('and the boy buy 6 egg')
     print('and budi go to home')
 else:
     print('budi no buy a milk')
 print('budi talk to mom')
+
