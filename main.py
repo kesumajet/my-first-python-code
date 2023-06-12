0ff0d516c09ba261ff490956d9bd981f8d140d44
Kusuma <kusumo999666@gmail.com>
2023-06-12 19:57:15 +0700

    initial commit

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ print('hello world')
 milk_base = 562
 egg_bake = 25
 
+# sequential
 if milk_base > 0:
     print('budi buy a milk')
     print('and budi go to home')
+else:
+    print('budi no buy a milk')
+print('budi talk to mom')
