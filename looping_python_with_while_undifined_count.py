04a7e573bd615b6e88274bf319f9c0d3e3642019
Kusuma <kusumo999666@gmail.com>
2023-06-14 23:12:27 +0700

    looping with while undefined 2

--- looping_python_with_while_undifined_count.py
+++ looping_python_with_while_undifined_count.py
@@ -3,7 +3,7 @@ Looping with while to understand
 """
 Book_count = 7
 count_read = 0
-print('Ibu berkata "Bacalah semua buku mu"')
+print('Mom said "Read and understand ur book"')
 
 Book_read_and_understand = 0
 print(f'already read and understand {Book_read_and_understand}')
@@ -17,3 +17,8 @@ while count_read < Book_count * 2:
         print(f'Book number {Book_read_and_understand} read and understand')
 
 print(f'Already read and understand {Book_read_and_understand}')
+if Book_read_and_understand == Book_count:
+    print('"mom, already read and understand')
+else:
+    print(f'no all book read and understand {Book_read_and_understand} Book')
+
