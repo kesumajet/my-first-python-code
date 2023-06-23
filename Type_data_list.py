6f9c07c1dc7f74b88ba044f8ce334f5a3b2c552e
Kusuma <kusumo999666@gmail.com>
2023-06-23 20:50:15 +0700

    Type data list

--- Type_data_list.py
+++ Type_data_list.py
@@ -21,3 +21,36 @@ print('\nAdd one book')
 book_list.append('Jalur Langit')
 for i in range(0, len(book_list)):
     print(book_list[i])
+
+print('\nClear List')
+book_list.clear()
+for i in range(0, len(book_list)):
+    print(book_list[i])
+
+print('\nGanti Element')
+book_list = ['Sebab Kita Semua Gila Sex', 'Mencari Cinta', 'Emha Ainun Najib']
+book_list[0] = 'eight hubby'
+for i in range(0, len(book_list)):
+    print(book_list[i])
+
+print('\nAmbil Element ke - 2')
+book = book_list.pop(1)
+for i in range(0, len(book_list)):
+    print(book_list[i])
+
+print('\nBuku yang diambil tadi')
+print(book)
+#.pop adalah mengambil dari list dan menyimpan dalam variabel
+#perintah pop juga dapat dipanggil tanpa parameter
+
+book_list = ['Sebab Kita Semua Gila Sex', 'Mencari Cinta', 'Emha Ainun Najib']
+print('\nPop')
+book_list.pop()
+for i in range(0, len(book_list)):
+    print(book_list[i])
+
+book_list = ['Sebab Kita Semua Gila Sex', 'Mencari Cinta', 'Emha Ainun Najib']
+print('\nPop -1')
+book_list.pop(-1)
+for i in range(0, len(book_list)):
+    print(book_list[i])
