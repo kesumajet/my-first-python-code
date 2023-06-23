1f4746cd39d8dad1b96f126ebae0a55033107158
Kusuma <kusumo999666@gmail.com>
2023-06-23 21:03:58 +0700

    Type data list comprehension

--- Type_data_list.py
+++ Type_data_list.py
@@ -54,3 +54,31 @@ print('\nPop -1')
 book_list.pop(-1)
 for i in range(0, len(book_list)):
     print(book_list[i])
+
+print('\nPerintah del')
+book_list = ['Sebab Kita Semua Gila Sex', 'Mencari Cinta', 'Emha Ainun Najib']
+del book_list[0]
+for i in range(0, len(book_list)):
+    print(book_list[i])
+
+print('\nPerintah del with command comprehension')
+book_list = ['Sebab Kita Semua Gila Sex', 'Mencari Cinta', 'Emha Ainun Najib']
+del book_list[:]
+for i in range(0, len(book_list)):
+    print(book_list[i])
+
+print('\nPerintah del with command comprehension start stop')
+book_list = ['Sebab Kita Semua Gila Sex', 'Mencari Cinta', 'Emha Ainun Najib']
+del book_list[0:1]
+#index dimulai dari 0 dan jumlah dari 1
+#depan adalah index
+#belakang adalah jumlah
+#juga bisa dari belakang dengan (-)
+for i in range(0, len(book_list)):
+    print(book_list[i])
+
+print('\nPerintah del with command comprehension start stop step')
+book_list = ['Sebab Kita Semua Gila Sex', 'Mencari Cinta', 'Emha Ainun Najib']
+del book_list[0::2]
+for i in range(0, len(book_list)):
+    print(book_list[i])
